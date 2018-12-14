c4ef519a22bc630851023d41661c13a227894ad9
root <root@ip-172-31-36-209.us-east-2.compute.internal>
2018-12-14 08:55:32 +0000

    added the comment line

--- welcome.java
+++ welcome.java
@@ -1,5 +1,10 @@
+
+
+//basic program
+
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");
+   System.out.println("welcome");
     }  
 }  
